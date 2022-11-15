ad4d02e4ab8f27c6a97f418e54a5ad4529be8963
William Gold <goldcorum@gmail.com>
2022-11-15 12:54:34 -0500

    Solve who likes it -- 1st

--- 6.whoLikesIt.js
+++ 6.whoLikesIt.js
@@ -11,8 +11,19 @@
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 // Note: For 4 or more names, the number in "and 2 others" simply increases.
 
-let solveCount = 0;
+let solveCount = 1;
 
 function likes(names) {
-  // TODO
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} other like this`;
+  }
 }
