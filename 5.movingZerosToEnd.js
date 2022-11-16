8da6ff84772d9f658edb984f940e9b676b2b2265
William Gold <goldcorum@gmail.com>
2022-11-16 16:17:02 -0500

    Solve moving Zeros to the end -- 1st

--- 5.movingZerosToEnd.js
+++ 5.movingZerosToEnd.js
@@ -1,9 +1,13 @@
+//Link: https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
 moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]); // returns[false,1,1,2,1,3,"a",0,0]
 
-let solveCount = 0;
+let solveCount = 1;
 
 function moveZeros(arr) {
-  return;
-}
+  return arr
+    .filter((char) => char !== 0)
+    .concat(arr.filter((char) => char === 0));
+}`¡`
