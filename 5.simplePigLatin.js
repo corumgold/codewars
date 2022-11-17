0c958e5f32c08178e85b3dc1936177b469fe665f
William Gold <goldcorum@gmail.com>
2022-11-17 10:09:35 -0500

    Add simplePigLatin starter code

---
+++ 5.simplePigLatin.js
@@ -0,0 +1,13 @@
+// Link: https://www.codewars.com/kata/520b9d2ad5c005041100000f
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+// Leave punctuation marks untouched.
+
+let solveCount = 0;
+
+function pigIt(str) {
+  //Code here
+}
+
+pigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+pigIt("Hello world !"); // elloHay orldway !
