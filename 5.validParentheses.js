4164f8188ede20321b3c10ad8dc4349d2d0cd35d
William Gold <goldcorum@gmail.com>
2022-11-21 10:23:39 -0500

    Add starter code for valid parentheses

---
+++ 5.validParentheses.js
@@ -0,0 +1,16 @@
+//Link: https://www.codewars.com/kata/52774a314c2333f0a7000688/train/javascript
+
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid.
+// The function should return true if the string is valid, and false if it's invalid.
+
+function validParentheses(parens) {
+  // your code here ..
+  return false;
+}
+
+validParentheses("()"); //  true
+validParentheses(")(()))"); //  false
+validParentheses("("); //  false
+validParentheses("(())((()())())"); //  true
+
+// 0 <= input.length <= 100
