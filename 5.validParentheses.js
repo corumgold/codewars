5b4e4d62abb42c9536a8f57e5ed35754dd6ce0d6
William Gold <goldcorum@gmail.com>
2022-11-21 10:54:32 -0500

    Add initial solution (invalid)

--- 5.validParentheses.js
+++ 5.validParentheses.js
@@ -3,9 +3,16 @@
 // Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid.
 // The function should return true if the string is valid, and false if it's invalid.
 
+let solveCount = 0;
+
 function validParentheses(parens) {
-  // your code here ..
-  return false;
+  let stack = [];
+  for (let i = 0; i < parens.length; i++) {
+    if (stack[stack.length - 1] === parens[i] || !stack[stack.length - 1]) {
+      stack.push(parens[i]);
+    } else stack.pop();
+  }
+  return stack.length ? false : true;
 }
 
 validParentheses("()"); //  true
