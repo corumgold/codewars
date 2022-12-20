28e39478ec9a0ea0fd4bddce4b6f1ceb94132c40
William Gold <goldcorum@gmail.com>
2022-12-20 13:59:39 -0800

    Add starter code for nexct bigger same digits

---
+++ 4.nextBiggerSameDigits.js
@@ -0,0 +1,24 @@
+//Link: https://www.codewars.com/kata/55983863da40caa2c900004e/train/javascript
+
+// Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+
+// 12 ==> 21
+// 513 ==> 531
+// 2017 ==> 2071
+nextBigger(12); // returns 21
+nextBigger(513); // returns 531
+nextBigger(2017); // returns 2071
+
+// If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift):
+
+// 9 ==> -1
+// 111 ==> -1
+// 531 ==> -1
+nextBigger(9); // returns -1
+nextBigger(111); // returns -1
+nextBigger(531); // returns -1
+
+const solveCount = 0;
+
+function nextBigger(n) {
+}
